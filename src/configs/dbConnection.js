d7c18f5cb1415a9f5d4de914fbcce1f39551f9fd
M Burak Yilmazer <mbyilmazer38@gmail.com>
2024-04-18 21:39:39 +0300

    up

--- src/configs/dbConnection.js
+++ src/configs/dbConnection.js
@@ -1,24 +1,25 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
 // MongoDB Connection:
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const dbConnection = function () {
-    // Connect:
-    mongoose.connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => {
-            console.log('* DB Connected * ')
-            // Syncronization (must be in commentLine):
-            // require('./sync')() // !!! It clear database.
-        })
-        .catch((err) => console.log('* DB Not Connected * ', err))
-}
+  // Connect:
+  mongoose
+    .connect(process.env.MONGODB)
+    .then(() => {
+      console.log("* DB Connected * ");
+      // Syncronization (must be in commentLine):
+      // require('./sync')() // !!! It clear database.
+    })
+    .catch((err) => console.log("* DB Not Connected * ", err));
+};
 
 /* ------------------------------------------------------- */
 module.exports = {
-    mongoose,
-    dbConnection
-} 
+  mongoose,
+  dbConnection,
+};
